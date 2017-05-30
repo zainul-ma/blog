0a534470e349cc41c95360178793680804b53a3d
zainul-ma <zainul.ma@tnis.com>
2017-05-30 08:56:30 +0700

    adding sample user test and modify default test

--- tests/user_test.go
+++ tests/user_test.go
@@ -19,8 +19,8 @@ func init() {
 }
 
 // TestGet is a sample to run an endpoint test
-func TestGet(t *testing.T) {
-	r, _ := http.NewRequest("GET", "/v1/object", nil)
+func TestUserGet(t *testing.T) {
+	r, _ := http.NewRequest("GET", "/v1/user", nil)
 	w := httptest.NewRecorder()
 	beego.BeeApp.Handlers.ServeHTTP(w, r)
 
@@ -36,13 +36,12 @@ func TestGet(t *testing.T) {
 	})
 }
 
-func TestGetById(t *testing.T) {
-	r, _ := http.NewRequest("GET", "/v1/object/hjkhsbnmn123", nil)
+func TestUserGetById(t *testing.T) {
+	r, _ := http.NewRequest("GET", "/v1/user/user_11111", nil)
 	w := httptest.NewRecorder()
 	beego.BeeApp.Handlers.ServeHTTP(w, r)
 
-	beego.Trace("testing by id", "TestGetById", "Code[%d]\n%s",
-		w.Code, w.Body.String())
+	beego.Trace("testing", "TestGet", "Code[%d]\n%s", w.Code, w.Body.String())
 
 	Convey("Subject: Test Station Endpoint\n", t, func() {
 		Convey("Status Code Should Be 200", func() {
@@ -54,13 +53,41 @@ func TestGetById(t *testing.T) {
 	})
 }
 
-func TestDeleteById(t *testing.T) {
-	r, _ := http.NewRequest("DELETE", "/v1/object/hjkhsbnmn123", nil)
+func TestUserDeleteById(t *testing.T) {
+	r, _ := http.NewRequest("DELETE", "/v1/user/user_11111", nil)
 	w := httptest.NewRecorder()
 	beego.BeeApp.Handlers.ServeHTTP(w, r)
 
-	beego.Trace("testing by id", "TestGetById", "Code[%d]\n%s",
-		w.Code, w.Body.String())
+	beego.Trace("testing", "TestGet", "Code[%d]\n%s", w.Code, w.Body.String())
+
+	Convey("Subject: Test Station Endpoint\n", t, func() {
+		Convey("Status Code Should Be 200", func() {
+			So(w.Code, ShouldEqual, 200)
+		})
+	})
+}
+
+func TestUserLogout(t *testing.T) {
+	r, _ := http.NewRequest("GET", "/v1/user/logout", nil)
+	w := httptest.NewRecorder()
+	beego.BeeApp.Handlers.ServeHTTP(w, r)
+
+	beego.Trace("testing", "TestGet", "Code[%d]\n%s", w.Code, w.Body.String())
+
+	Convey("Subject: Test Station Endpoint\n", t, func() {
+		Convey("Status Code Should Be 200", func() {
+			So(w.Code, ShouldEqual, 200)
+		})
+	})
+}
+
+func TestUserLogin(t *testing.T) {
+	r, _ := http.NewRequest("GET", "/v1/user/login?username=abc&password=123456",
+		nil)
+	w := httptest.NewRecorder()
+	beego.BeeApp.Handlers.ServeHTTP(w, r)
+
+	beego.Trace("testing", "TestGet", "Code[%d]\n%s", w.Code, w.Body.String())
 
 	Convey("Subject: Test Station Endpoint\n", t, func() {
 		Convey("Status Code Should Be 200", func() {
